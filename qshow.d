276f045b41aac2569b92005d53debcf7875a83e4
Kazuki KOMATSU <komatsu@comm.ee.tut.ac.jp>
2019-12-15 12:49:39 +0900

    各情報を構造体にまとめた

--- qshow.d
+++ qshow.d
@@ -143,16 +143,79 @@ string replaceFmtString(string fmt, in string[] arglist)
 }
 
 
-alias showNodesColumnNames = AliasSeq!("vnode", "njobs", "ncpus f/t", "mem f/t", "gpu", "users", "state");
-immutable showNodesFmtList = ["name", "njobs", "cpu", "mem", "gpu", "users", "state"];
+struct InfoTag
+{
+    string ident;
+    string name;
+}
+
+
+struct NodeInfo
+{
+    @InfoTag("name",   "vnode")         string vnode;
+    @InfoTag("njobs",  "njobs")         long njobs;
+    @InfoTag("cpu",    "ncpus f/t")     string cpu;
+    @InfoTag("mem",    "mem f/t")       string mem;
+    @InfoTag("gpu",    "gpu")           string gpu;
+    @InfoTag("users",  "users")         Tuple!(string, long)[] users;
+    @InfoTag("state",  "state")         string state;
+}
+
+
+struct UserInfo
+{
+    @InfoTag("user",   "Username")      string user;
+    @InfoTag("tjob",   "tJob")          long tjob;
+    @InfoTag("tcpu",   "tCPU")          long tcpu;
+    @InfoTag("tmem",   "tMem")          string tmem;
+    @InfoTag("rjob",   "rJob")          long rjob;
+    @InfoTag("rcpu",   "rCPU")          long rcpu;
+    @InfoTag("rmem",   "rMem")          string rmem;
+}
+
+struct JobInfo
+{
+    @InfoTag("id",         "Job ID")        string id;
+    @InfoTag("user",       "Username")      string user;
+    @InfoTag("S",          "S")             string state;
+    @InfoTag("tcpu",       "tCPU")          long tcpu;
+    @InfoTag("tmem",       "tMem")          string tmem;
+    @InfoTag("rmem",       "rMem")          string rmem;
+    @InfoTag("vmem",       "vMem")          string vmem;
+    @InfoTag("cpup",       "CPU(%)")        long cpup;
+    @InfoTag("cput",       "CPU Time")      string cputime;
+    @InfoTag("walltime",   "Walltime")      string walltime;
+    @InfoTag("name",       "Job Name")      string name;
+    @InfoTag("queue",      "Queue")         string queue;
+    @InfoTag("C",          "C")             string C;
+    @InfoTag("container",  "Container")     string containerType;
+    @InfoTag("image",      "Image")         string image;
+}
+
+string[] getIdentNames(T)()
+{
+    string[] dst;
+    static foreach(sym; getSymbolsByUDA!(T, InfoTag)) {
+        dst ~= getUDAs!(sym, InfoTag)[0].ident;
+    }
+
+    return dst;
+}
+
+
+string[] getColumnNames(T)()
+{
+    string[] dst;
+    static foreach(sym; getSymbolsByUDA!(T, InfoTag)) {
+        dst ~= getUDAs!(sym, InfoTag)[0].name;
+    }
+
+    return dst;
+}
+
+
 immutable defaultShowNodesFmt = "%name:6s  %state:8s  %njobs:5s  %cpu:9s  %mem:11s  %gpu:3s  %users:(%(%7s*%2d%), %)";
-
-alias showUsersColumnNames = AliasSeq!("Username", "tJob", "tCPU", "tMem", "rJob", "rCPU", "rMem");
-immutable showUsersFmtList = ["user", "tjob", "tcpu", "tmem", "rjob", "rcpu", "rmem"];
 immutable defaultShowUsersFmt = "%user:10s  %tjob:4s  %tcpu:4s  %tmem:8s  %rjob:4s  %rcpu:4s  %rmem:8s";
-
-alias showJobsColumnNames = AliasSeq!("Job ID", "Username", "S", "tCPU", "tMem", "rMem", "vMem", "CPU(%)", "CPU Time", "Walltime", "Jobname", "Queue", "C", "Container", "Image");
-immutable showJobsFmtList = ["id", "user", "S", "tcpu", "tmem", "rmem", "vmem", "cpup", "cput", "walltime", "name", "queue", "C", "container", "image"];
 immutable defaultShowJobsFmt =  "%id:10s  %user:10s  %queue:6s  %name:20s  %S:1s  %tcpu:4s  %tmem:8s  %rmem:8s  %vmem:8s  %cpup:6s  %cput:10s  %walltime:10s  %C:1s  %image:20s";
 
 
@@ -212,19 +275,19 @@ void main(string[] args)
 
 
     if(showNodes) {
-        fmtShowNode = fmtShowNode.replaceFmtString(showNodesFmtList);
+        fmtShowNode = fmtShowNode.replaceFmtString(getIdentNames!NodeInfo);
         showNodeInfo(nodeList, jobList, fmtShowNode, showColored);
         if(showUsers || showJobs) writeln();
     }
 
     if(showUsers) {
-        fmtShowUsers = fmtShowUsers.replaceFmtString(showUsersFmtList);
+        fmtShowUsers = fmtShowUsers.replaceFmtString(getIdentNames!UserInfo);
         showUserInfo(nodeList, jobList, fmtShowUsers, showColored);
         if(showJobs) writeln();
     }
 
     if(showJobs) {
-        fmtShowJobs = fmtShowJobs.replaceFmtString(showJobsFmtList);
+        fmtShowJobs = fmtShowJobs.replaceFmtString(getIdentNames!JobInfo);
         showJobInfo(nodeList, jobList, fmtShowJobs, showOnlyMyJobs, showColored);
     }
 }
@@ -233,7 +296,7 @@ void main(string[] args)
 void showNodeInfo(in JSONValue[] nodeList, in JSONValue[] jobList, string fmtstr, bool showColored)
 {
     if(! dontShowHeader) {
-        writeValues(stdout.lockingTextWriter, true, fmtstr, showNodesColumnNames);
+        writeValues(stdout.lockingTextWriter, true, fmtstr, aliasSeqOf!(getColumnNames!NodeInfo()) );
         writeln();
         writeHyphen(stdout.lockingTextWriter, fmtstr);
         writeln();
@@ -247,13 +310,14 @@ void showNodeInfo(in JSONValue[] nodeList, in JSONValue[] jobList, string fmtstr
     }
 
     foreach(node; nodeList) {
-        auto name = node["key"].str;
+        NodeInfo info;
+        info.vnode = node["key"].str;
         auto jobids = node["jobs"].array.map!(a => a.str.replace(".xregistry0", "")).array().sort.uniq.array();
-        auto njob = jobids.length;
-        auto mem = node["mem f/t"].str;
-        auto ncpus = node["ncpus f/t"].str;
-        auto ngpus = node["ngpus f/t"].str;
-        auto state = node["State"].str;
+        info.njobs = jobids.length;
+        info.mem = node["mem f/t"].str;
+        info.cpu = node["ncpus f/t"].str;
+        info.gpu = node["ngpus f/t"].str;
+        info.state = node["State"].str;
 
         long[string] userCPUs;
         foreach(jobid; jobids) {
@@ -287,14 +351,14 @@ void showNodeInfo(in JSONValue[] nodeList, in JSONValue[] jobList, string fmtstr
             }
         }
 
-        auto usercpus = userCPUs.byKeyValue.map!(a => tuple(a.key, a.value)).array();
+        info.users = userCPUs.byKeyValue.map!(a => tuple(a.key, a.value)).array();
 
         auto fmt = fmtstr;
-        if(showColored && (state != "free" || ncpus.startsWith("0/") || mem.startsWith("0gb/")) ) {
+        if(showColored && (info.state != "free" || info.cpu.startsWith("0/") || info.mem.startsWith("0gb/")) ) {
             fmt = fmt.color(fg.red);
         }
 
-        writeValues(stdout.lockingTextWriter, false, fmt, name, njob, ncpus, mem, ngpus, usercpus, state);
+        writeValues(stdout.lockingTextWriter, false, fmt, info.tupleof);
         writeln();
     }
 }
@@ -306,7 +370,7 @@ void showUserInfo(in JSONValue[] nodeList, in JSONValue[] jobList, string fmtstr
     auto userList = jobList.map!q{a["Variable_List"]["PBS_O_LOGNAME"].str}.array().sort().uniq.array();
 
     if(! dontShowHeader) {
-        writeValues(stdout.lockingTextWriter, true, fmtstr, showUsersColumnNames);
+        writeValues(stdout.lockingTextWriter, true, fmtstr, aliasSeqOf!(getColumnNames!UserInfo()));
         writeln();
         writeHyphen(stdout.lockingTextWriter, fmtstr);
         writeln();
@@ -320,6 +384,8 @@ void showUserInfo(in JSONValue[] nodeList, in JSONValue[] jobList, string fmtstr
     }
 
     foreach(user; userList) {
+        UserInfo info;
+        info.user = user;
         auto totJobs = jobList.removeTerminatedJob().getUserJobs(user).array();
         auto runJobs = totJobs.filter!q{a["job_state"].str == "R"}.array();
 
@@ -333,11 +399,19 @@ void showUserInfo(in JSONValue[] nodeList, in JSONValue[] jobList, string fmtstr
         auto totResource = aggregate(totJobs);
         auto runResource = aggregate(runJobs);
 
+        info.tjob = totResource[0];
+        info.tcpu = totResource[1];
+        info.tmem = totResource[2];
+
+        info.rjob = runResource[0];
+        info.rcpu = runResource[1];
+        info.rmem = runResource[2];
+
         auto fmt = fmtstr;
-        if(showColored && user == environment["USER"])
+        if(showColored && info.user == environment["USER"])
             fmt = fmt.color(fg.green);
 
-        writeValues(stdout.lockingTextWriter, false, fmt, user, totResource.tupleof, runResource.tupleof);
+        writeValues(stdout.lockingTextWriter, false, fmt, info.tupleof);
         writeln();
     }
 }
@@ -348,7 +422,7 @@ void showJobInfo(in JSONValue[] nodeList, in JSONValue[] jobList, string fmtstr,
     auto currUser = environment["USER"];
 
     if(! dontShowHeader) {
-        writeValues(stdout.lockingTextWriter, true, fmtstr, showJobsColumnNames);
+        writeValues(stdout.lockingTextWriter, true, fmtstr, aliasSeqOf!(getColumnNames!JobInfo()));
         writeln();
         writeHyphen(stdout.lockingTextWriter, fmtstr);
         writeln();
@@ -362,56 +436,53 @@ void showJobInfo(in JSONValue[] nodeList, in JSONValue[] jobList, string fmtstr,
     }
 
     foreach(job; jobList.dup.jobSort()) {
+        JobInfo info;
         // writeln(job["key"]);
-        string id = job["key"].str;
-        long tcpu = job["Resource_List"]["ncpus"].integer;
-        string tmem = job["Resource_List"]["mem"].str.toGB;
-        string jobS = job["job_state"].str;
-        string user = job["Variable_List"]["PBS_O_LOGNAME"].str;
-        string jobname = job["Job_Name"].str;
-        string queue = job["queue"].str;
-        string containerType;
-        string containerImage;
+        info.id = job["key"].str;
+        info.tcpu = job["Resource_List"]["ncpus"].integer;
+        info.tmem = job["Resource_List"]["mem"].str.toGB;
+        info.state = job["job_state"].str;
+        info.user = job["Variable_List"]["PBS_O_LOGNAME"].str;
+        info.name = job["Job_Name"].str;
+        info.queue = job["queue"].str;
         if("SINGULARITY_IMAGE" in job["Variable_List"]) {
-            containerType = "Singularity";
-            containerImage = job["Variable_List"]["SINGULARITY_IMAGE"].str;
+            info.containerType = "Singularity";
+            info.image = job["Variable_List"]["SINGULARITY_IMAGE"].str;
         } else if ("DOCKER_IMAGE" in job["Variable_List"]) {
-            containerType = "Docker";
-            containerImage = job["Variable_List"]["DOCKER_IMAGE"].str;
+            info.containerType = "Docker";
+            info.image = job["Variable_List"]["DOCKER_IMAGE"].str;
         }
+        info.C = info.containerType[0 .. 1];
 
         // アレイジョブのうち，終わっているジョブの表示はしない
-        if(jobS == "X")
+        if(info.state == "X")
             continue;
 
         // -mオプションが渡されたときは自身のジョブ以外は表示しない
-        if(showOnlyMyJobs && user != currUser)
+        if(showOnlyMyJobs && info.user != currUser)
             continue;
 
-        string rmem = "?";
-        string vmem = "?";
-        long cpupercent = 0;
-        string cputime = "----:--:--";
-        string walltime = "----:--:--";
-        if(jobS == "R") {
-            rmem = job["resources_used"]["mem"].str.toGB;
-            vmem = job["resources_used"]["vmem"].str.toGB;
-            cpupercent = job["resources_used"]["cpupercent"].integer;
-            cputime = job["resources_used"]["cput"].str;
-            walltime = job["resources_used"]["walltime"].str;
+        info.rmem = "?";
+        info.vmem = "?";
+        info.cpup = 0;
+        info.cputime = "----:--:--";
+        info.walltime = "----:--:--";
+        if(info.state == "R") {
+            info.rmem = job["resources_used"]["mem"].str.toGB;
+            info.vmem = job["resources_used"]["vmem"].str.toGB;
+            info.cpup = job["resources_used"]["cpupercent"].integer;
+            info.cputime = job["resources_used"]["cput"].str;
+            info.walltime = job["resources_used"]["walltime"].str;
         }
 
         auto fmt = fmtstr;
-        if(showColored && showOnlyMyJobs && jobS == "R") {
+        if(showColored && showOnlyMyJobs && info.state == "R") {
             fmt = fmt.color(fg.green);
-        } else if(showColored && !showOnlyMyJobs && user == currUser) {
+        } else if(showColored && !showOnlyMyJobs && info.user == currUser) {
             fmt = fmt.color(fg.green);
         }
 
-        writeValues(stdout.lockingTextWriter, false,
-            fmt, id, user, jobS, tcpu, tmem,
-            rmem, vmem, cpupercent, cputime, walltime, jobname, queue,
-            containerType[0 .. 1], containerType, containerImage);
+        writeValues(stdout.lockingTextWriter, false, fmt, info.tupleof);
         writeln();
     }
 }
